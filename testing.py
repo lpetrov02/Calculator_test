fec4ad9d6c0074043ce6a3d182e288bee405f7b9
lpetrov02 <71082527+lpetrov02@users.noreply.github.com>
2020-09-25 00:02:33 +0300

    Add files via upload
    It's late for 7 minutes but now it works better - i have realised "rounding"

--- testing.py
+++ testing.py
@@ -54,7 +54,8 @@ class TestCalculator(unittest.TestCase):
     def test_pow_rational(self):
         self.calculator = Calculator()
         calc_value = self.calculator.value
-        self.assertEqual(self.calculator.power(3.14).value, calc_value ** 3.14)
+        self.assertEqual(float('{:.10f}'.format(self.calculator.power(3.14).value)),
+                         float('{:.10f}'.format((calc_value ** 3.14))))
 
     def test_pow_incorrect_input(self):
         self.calculator = Calculator()
@@ -64,7 +65,8 @@ class TestCalculator(unittest.TestCase):
     def test_root(self):
         self.calculator = Calculator()
         calc_value = self.calculator.value
-        self.assertEqual(self.calculator.root(25).value, calc_value ** 0.04)
+        self.assertEqual(float('{:.10f}'.format(self.calculator.root(25).value)),
+                         float('{:.10f}'.format(calc_value ** 0.04)))
 
     def test_root_zero(self):
         self.calculator = Calculator()
@@ -74,30 +76,34 @@ class TestCalculator(unittest.TestCase):
     def test_root_less_zero(self):
         self.calculator = Calculator()
         calc_value = self.calculator.value
-        self.assertEqual(self.calculator.root(-10).value, 1.0 / calc_value ** 0.1)
+        self.assertEqual(float('{:.10f}'.format(self.calculator.root(-10).value)),
+                         float('{:.10f}'.format(1.0 / calc_value ** 0.1)))
 
     def test_root_rational(self):
         self.calculator = Calculator()
         calc_value = self.calculator.value
-        self.assertEqual(self.calculator.root(2.7).value, calc_value ** (1.0 / 2.7))
+        self.assertEqual(float('{:.10f}'.format(self.calculator.root(2.7).value)),
+                         float('{:.10f}'.format(calc_value ** (1.0 / 2.7))))
 
     def test_root_large(self):
         self.calculator = Calculator()
         calc_value = self.calculator.value
-        self.assertEqual(self.calculator.root(1000).value, calc_value ** 0.001)
+        self.assertEqual(float('{:.10f}'.format(self.calculator.root(1000).value)),
+                         float('{:.10f}'.format(calc_value ** 0.001)))
 
     def test_multy_test1(self):
         self.calculator = Calculator()
         calc_value = self.calculator.value
-        self.assertEqual(self.calculator.add(30, 239).divide(20, 30, integer_divide=False).
-                         power(7, 8, 350).subtract(1000000).value,
-                         ((((calc_value + 269) / 600.0) ** 7) ** 8) - 1000000)
+        self.assertEqual(float('{:.10f}'.format(self.calculator.add(30, 239).divide(20, 30, integer_divide=False).
+                         power(7, 8, 350).subtract(1000000).value)),
+                         float('{:.10f}'.format(((((calc_value + 269) / 600.0) ** 7) ** 8) - 1000000)))
 
     def test_multy_test2(self):
         self.calculator = Calculator()
         calc_value = self.calculator.value
-        self.assertEqual(self.calculator.root(3, 5).multiply(10, 20, 30).subtract(10, 100, 1000000).power(4).value,
-                         ((((calc_value ** (1.0 / 3.0)) ** (1.0 / 5.0)) * 6000.0) - 1000110) ** 4)
+        self.assertEqual(float('{:.10f}'.format(self.calculator.root(3, 5).multiply(10, 20, 30).
+                                                subtract(10, 100, 1000000).power(4).value)),
+                         float('{:.10f}'.format(((((calc_value ** (1.0 / 3.0)) ** (1.0 / 5.0)) * 6000.0) - 1000110) ** 4)))
 
     def test_time2(self):
         t1 = time.time()
